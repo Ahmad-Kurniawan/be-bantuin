899e5d144f770d563e366db8232f1178a1663eb8
rel1vee <muhzakierbaisyas@gmail.com>
2025-11-17 07:19:30 +0700

    test: enhance unit tests by adding mock services for controllers and services across the application

--- src/admin/admin.service.spec.ts
+++ src/admin/admin.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { WalletsService } from '../wallets/wallets.service';
+import { NotificationsService } from '../notifications/notifications.service';
 
 describe('AdminService', () => {
   let service: AdminService;
 
+  const mockPrismaService = {};
+  const mockWalletsService = {};
+  const mockNotificationsService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminService],
+      providers: [
+        AdminService,
+        { provide: PrismaService, useValue: mockPrismaService },
+        { provide: WalletsService, useValue: mockWalletsService },
+        { provide: NotificationsService, useValue: mockNotificationsService },
+      ],
     }).compile();
 
     service = module.get<AdminService>(AdminService);
